8b24717419e263da15ba986f698f6d7c8eeb6480
wufelix <cat5530xm@gmail.com>
2016-06-09 11:32:33 +0800

    [SET]EventInfoServlet.java add sexRatio into request

--- WEB-INF/src/EventInfoServlet.java
+++ WEB-INF/src/EventInfoServlet.java
@@ -16,33 +16,32 @@ public class EventInfoServlet extends HttpServlet{
 				response.setContentType("text/html; charset=utf-8");
 				PrintWriter out = response.getWriter();
 				ArrayList<Event> eList = (ArrayList<Event>) getServletContext().getAttribute("event");
-				String action = (String)request.getAttribute("action");
+				String action = (String)request.getAttribute("action");//check if top10
+				DataAnalysis da = new DataAnalysis();
 				
 				/****find top 10****/
 				if(action!=null){
-					DataAnalysis da = new DataAnalysis();
 					ArrayList<Event> top10= new ArrayList<Event>();
 					top10 = da.getHitRatio(eList);
 					request.setAttribute("top10",top10);
-					RequestDispatcher view = request.getRequestDispatcher("new.jsp");
+					RequestDispatcher view = request.getRequestDispatcher("top10.jsp");
 					view.forward(request,response); //next web
 				}
 				else{
 					
 				/****send event information ****/
-					
-					
 					int id = (int)request.getAttribute("id");//get id of redirect event
 					Event event = eList.get(id);
 					request.setAttribute("event",event);
 				
 				/****send the number of participatants ****/
+					request.setAttribute("participatants",(int)event.getApplyList.size());
 				
-					event.applyListName//how to get applyList linking with the event ?
+				/****send the sex ratio ****/
+					
+					Double ratio = da.getSexRatio(event.getApplyList);
+					request.setAttribute("sexRatio",ratio);
 				
-				
-				
-					request.setAttribute("participatants",(int)applyList.size());
 				
 					RequestDispatcher view = request.getRequestDispatcher("eventInfo.jsp");
 					view.forward(request,response); //next web
---
+++ top10.jsp
@@ -0,0 +1,20 @@
+<%@page contentType="text/html" pageEncoding="utf-8"%>
+<%@ page import="java.util.*" %>
+<%@ page import="com.model.*" %>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html"; charset = "utf-8">
+        <title>TOP10</title>	
+	</head>
+	<body>
+	ArrayList<Event> list = (ArrayList<Event>)request.getAttribute("top10");
+	
+	
+	
+	
+	</body>
+	
+	
+</html>
