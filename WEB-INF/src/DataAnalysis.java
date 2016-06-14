b6271df69c06f4cc7726848bae6cdc8b0ac32490
wufelix <cat5530xm@gmail.com>
2016-06-14 23:20:26 +0800

    [FIX]DataAnalysis lose function recovery

--- WEB-INF/src/DataAnalysis.java
+++ WEB-INF/src/DataAnalysis.java
@@ -2,6 +2,8 @@ package com.model;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class DataAnalysis{
 
@@ -46,17 +48,62 @@ public class DataAnalysis{
 
 
 	public ArrayList<Event> whatIParticipateIn(String kwd) {
+		ArrayList<Event> result = new ArrayList<Event>();
+		EventProcess ep = (EventProcess)getServletContext().getAttribute("event");
+		
+		ArrayList<Event> allEvents = ep.getAllEvents();	// here would be error because of the method name in EventProcess to get all the events.
+		for(Event e : allEvents){
+			ArrayList<Applier> allAppliers = e.getAllApplier();	// here would be error because of the method name in Event to get all the appliers.
+			for(Applier a : allAppliers){
+				if(a.getNumber().equals(me)){ // here would be error because of the method name in Applier to get its id(or called number).
+					result.add(e); break; 
+				}
+			}
+		}
+		
+		return result;		
 	}
 
-	public Hashtable<String,ArrayList<Event>> relationDistanceTable(ArrayList<Event> myEvents) {
+	public Hashtable<String, ArrayList<Event>> relationDistanceTable(ArrayList<Event> allMyEvents){
+		Hashtable<String, ArrayList<Event>> result = new Hashtable<String, ArrayList<Event>>();
+		
+		for(Event e : allMyEvents){
+			ArrayList<Applier> allAppliers = e.getAllApplier();	// here would be error because of the method name in Event to get all the appliers.
+			for(Applier a : allAppliers){
+				String id = a.getNumber(); // here would be error because of the method name in Applier to get its id(or called number).
+				ArrayList<Event> events;
+				if(!result.containsKey(id)) events = new ArrayList<Event>();
+				else events = result.get(id);
+				events.add(e);
+				result.put(id, events);
+			}
+		}
+		
+		return result;
+	}
+	
+	
 
+	public RelationJsonPack RelationJsonPacker(String me, Hashtable<String, ArrayList<Event>> table){
+		ArrayList<LittleJsonPack> ljpList = new ArrayList<LittleJsonPack>();
+		LittleJsonPack ljp = new LittleJsonPack(me, new ChildJsonPack(me, 40));
+		ljpList.add(ljp);
+		for(String key : table.keySet()){
+			int size = table.get(key).size() * 2;
+			if(size > 20) size = 20;
+			ljp = new LittleJsonPack(key, new ChildJsonPack(key, size));
+			ljpList.add(ljp);
+		}
 	}
 
-	public void RelationJsonPacker(String kwd, Hashtable<String, ArrayList<Event>> table) {
-		return;
-	}
-
-	public void Relation2JsonFile(void aVoid, String fn) {
-		return;
+	public void Relation2JsonFile(RelationJsonPack rjp, String fileName) throws IOException {
+		
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(rjp);
+		
+		File flare = new File(fileName);
+		FileWriter fw = new FileWriter(flare, false);
+		fw.write(jsonStr);
+		fw.close();
 	}
 }
